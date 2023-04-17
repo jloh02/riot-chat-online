70470a055ce96b981a00b46762a506a14b1ef16f
Jonathan Loh <jonathanloh02@gmail.com>
2023-04-17 21:10:13 +0800

    Add globals

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,9 +10,4 @@ app.use(morgan("dev"));
 
 app.get("/ping", ping);
 
-app.post("/auth/:id", (req, res) => {
-  console.log(req.body);
-  res.sendStatus(200);
-});
-
 export default app;
---
+++ backend/src/globals.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { Agent } from "https";
+import { exit } from "process";
+
+export let buildNumber: string, riotClientVersion: string;
+
+axios
+  .get("https://valorant-api.com/v1/version")
+  .then((verRes) => {
+    buildNumber = verRes.data.data.riotClientBuild;
+    riotClientVersion = verRes.data.data.version;
+  })
+  .catch((err) => {
+    console.error("Unable to get client build version: ", err);
+    exit(1);
+  });
+
+const CIPHERS = [
+  "TLS_CHACHA20_POLY1305_SHA256",
+  "TLS_AES_128_GCM_SHA256",
+  "TLS_AES_256_GCM_SHA384",
+  "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
+];
+const CUSTOM_AGENT = new Agent({
+  ciphers: CIPHERS.join(":"),
+  honorCipherOrder: true,
+  minVersion: "TLSv1.2",
+});
+
+export const axiosFetch = axios.create({
+  headers: {
+    "Content-Type": "application/json",
+    "User-Agent": `RiotClient/${buildNumber} rso-auth (Windows; 10;;Professional, x64)`,
+  },
+  httpsAgent: CUSTOM_AGENT,
+  withCredentials: true,
+});
