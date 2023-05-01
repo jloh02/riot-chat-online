d8fa3940ef13f9d18a736f485e860781cc062471
Jonathan Loh <jonathanloh02@gmail.com>
2023-05-01 19:40:19 +0800

    Add domain into config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,6 +4,7 @@ config();
 
 let conf = {
   PORT: process.env.PORT || "3000",
+  DOMAIN: process.env.DOMAIN || "localhost:3000",
   SSL_PASSPHRASE: process.env.SSL_PASSPHRASE,
   BUILD_REFRESH_FREQUENCY: parseInt(process.env.BUILD_REFRESH_FREQUENCY), //Get client build version every...
 };
