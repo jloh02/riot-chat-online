c80d7ccf24210c149a30425195b54ff8d19bbbfe
ArcusO0 <marcusloh05@gmail.com>
2023-05-24 22:20:59 +0800

    Move frontend to root

--- frontend/src/pages/chat.tsx
+++
@@ -1,8 +0,0 @@
-import "../App.css";
-function Chat() {
-    console.log("test")
-    return (<>
-        <p style={{ Color: "black", }}>chat page</p>
-    </>)
-};
-export default Chat;
---
+++ src/pages/chat.tsx
@@ -0,0 +1,8 @@
+import "../App.css";
+function Chat() {
+    console.log("test")
+    return (<>
+        <p style={{ Color: "black", }}>chat page</p>
+    </>)
+};
+export default Chat;
